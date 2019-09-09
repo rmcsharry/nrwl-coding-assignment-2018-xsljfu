dcc9971d6db93b932bb7799d6c4f1ad0ee2edb4f
Richard McSharry <rmcsharry@gmail.com>
2019-09-09 18:32:44 +0200

    Fixes the dependency versions for material

---
+++ src/app/custom-material/custom-material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCheckboxModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule
+  ],
+})
+export class CustomMaterialModule { }
