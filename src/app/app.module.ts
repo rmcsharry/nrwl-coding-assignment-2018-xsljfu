cae795cf4c7ede4004df345d94d39b657dd157a7
Richard McSharry <rmcsharry@gmail.com>
2019-09-09 18:53:25 +0200

    Adds basic material list navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BackendService } from './backend.service';
+import { CustomMaterialModule } from './custom-material/custom-material.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { BackendService } from './backend.service';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CustomMaterialModule
   ],
   providers: [BackendService],
   bootstrap: [AppComponent]
--- src/app/custom-material/custom-material.module.ts
+++ src/app/custom-material/custom-material.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {MatButtonModule} from '@angular/material/button';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatListModule } from '@angular/material/list'
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule,
     MatCheckboxModule,
+    MatListModule
   ],
   exports: [
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatListModule
   ],
 })
 export class CustomMaterialModule { }
